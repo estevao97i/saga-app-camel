2c56db80a3640c72ca9199400aa06e288a346b4d
estevo <pedro.cordova2002@gmail.com>
2023-09-14 22:58:49 -0300

    create first saga addPedidoService

--- code-with-quarkus/src/main/java/org/acme/SagaRoute.java
+++ code-with-quarkus/src/main/java/org/acme/SagaRoute.java
@@ -1,4 +1,31 @@
 package org.acme;
 
-public class SagaRoute {
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.SagaPropagation;
+import org.apache.camel.saga.CamelSagaService;
+import org.apache.camel.saga.InMemorySagaService;
+
+@ApplicationScoped
+public class SagaRoute extends RouteBuilder {
+
+    @Inject
+    CreditoService creditoService;
+
+    @Inject
+    PedidoService pedidoService;
+
+    @Override
+    public void configure() throws Exception {
+
+        CamelSagaService sagaService = new InMemorySagaService();
+        getContext().addService(sagaService);
+
+        from("direct:saga").saga().propagation(SagaPropagation.REQUIRES_NEW).log("Iniciando transação")
+                .to("direct:newPedido").log("Criando pedido")
+                .to("direct:newPedidoValor").log("Reservando crédito")
+                .to("direct:finaliza").log("Feito!");
+
+    }
 }
