8b42c3f9b0955c3ce32dba3f0c5015ffcfa6e06f
estevo <pedro.cordova2002@gmail.com>
2023-09-14 22:47:39 -0300

    criacao sagaRoute

--- code-with-quarkus/src/main/java/org/acme/CreditoService.java
+++ code-with-quarkus/src/main/java/org/acme/CreditoService.java
@@ -1,4 +1,34 @@
 package org.acme;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ApplicationScoped // para injetar essa classe em outra
 public class CreditoService {
+
+    private int creditoTotal;
+    private Map<Long, Integer> pedido_valor = new HashMap<>();
+
+    public CreditoService() {
+        creditoTotal = 100;
+    }
+
+    public void newPedidoValor(Long pedidoId, int valor) {
+        if (valor > creditoTotal) {
+            throw new IllegalStateException("Saldo insuficiente");
+        }
+        creditoTotal = creditoTotal - valor;
+        pedido_valor.put(pedidoId, valor);
+    }
+
+    public void cancelPedidoValor(Long id) {
+        creditoTotal = creditoTotal + pedido_valor.get(id);
+        pedido_valor.remove(id);
+    }
+
+    public int getCreditoTotal() {
+        return creditoTotal;
+    }
 }
