bebca955da4c045b9307bc64c95a253fd9aac253
estevo <pedro.cordova2002@gmail.com>
2023-09-16 13:30:35 -0300

    alterando pedido para microservice

--- saga-distr/camel-app-pedido/src/main/java/org/acme/PedidoResource.java
+++ saga-distr/camel-app-pedido/src/main/java/org/acme/PedidoResource.java
@@ -4,9 +4,9 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.apache.camel.CamelContext;
 
 @Path("pedido")
 public class PedidoResource {
@@ -14,22 +14,21 @@ public class PedidoResource {
     @Inject
     PedidoService service;
 
-    @Path("newPedido")
     @GET
+    @Path("newPedido")
     @Produces(MediaType.TEXT_PLAIN)
-    public Response newPedido(Long id) {
+    public Response newPedido(@QueryParam("id") Long id) {
         service.newPedido(id);
-
-        return Response.ok().build();
+        return Response.status(Response.Status.CREATED).entity(id).build();
     }
 
-    @Path("cancelPedido")
     @GET
+    @Path("cancelPedido")
     @Produces(MediaType.TEXT_PLAIN)
-    public Response cancelPedido(Long id) {
+    public Response cancelPedido(@QueryParam("id") Long id) {
         service.cancelPedido(id);
 
-        return Response.ok().build();
+        return Response.noContent().entity(id).build();
     }
 
 
